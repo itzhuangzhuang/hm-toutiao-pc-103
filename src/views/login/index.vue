9544df83b706a80691dcf518d7c9f6bc379cef10
xz <1297301103@qq.com>
2020-08-17 22:52:08 +0800

    后端111

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -17,6 +17,7 @@
         </el-form-item>
         <el-form-item>
           <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+		  11111111111111111
         </el-form-item>
         <el-form-item>
           <el-button @click="login()" type="primary" style="width:100%">登 录</el-button>
